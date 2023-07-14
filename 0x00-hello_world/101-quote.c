7abf3fceed4f8ab9b0890c0d12cef46fcd021c00
erasekhilyass <eraskhilyass@gmail.com>
2023-07-14 08:15:29 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main - a c pro that prints a line to the standard error
-  * Return 1 (Success)
+  * main - a c program that prints a line to the standard error
+  *Return 1
   */
 int main(void)
 {
-	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
