d3053a770916952f8477e6e5974e008b092121ba
erasekhilyass <eraskhilyass@gmail.com>
2023-07-17 09:08:09 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+#define OUT stdout
 /**
   *main - Entry point
   *Return: always 0
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", fp);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", OUT);
 	return (1);
 }
