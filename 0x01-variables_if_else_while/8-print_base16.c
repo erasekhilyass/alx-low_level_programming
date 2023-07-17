2a8426c4323d769833f9aca1a2250483dd305927
erasekhilyass <eraskhilyass@gmail.com>
2023-07-16 18:16:53 -0700

    h

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Description: 'base 16'
+  *Return: 0
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; n <= 102; m++)
+	{
+		putchar(m);
+	}
+	return (0);
+}
