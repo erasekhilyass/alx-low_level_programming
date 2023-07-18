a4735ee70207f6fe8870302d873ade321c23e2b6
erasekhilyass <eraskhilyass@gmail.com>
2023-07-18 08:48:42 -0700

    h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_sign - Determines if the input is greater than, equal or less than 0.
+  * @c: the input number as an integer.
+  * Return: 1 is greater than 0. 0 is zero. -1 is less than 0.
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
