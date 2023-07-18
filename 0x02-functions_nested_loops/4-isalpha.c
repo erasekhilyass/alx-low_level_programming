b4852ae26394b5713d15da37fb01b5e18b2a694b
erasekhilyass <eraskhilyass@gmail.com>
2023-07-18 08:38:00 -0700

    h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _isalpha - shows 1 if it is a letter
+  * another case. shows 0
+  * @c: the character is ASCII code.
+  * Return: 1 for letters. 0 for the rest.
+  */
+int _isalpha(int c)
+{
+	if ((c >= 97 && <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
